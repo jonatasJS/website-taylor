72162a5b476b3b7f5397112fe794954f5328574d
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2022-01-27 01:06:49 -0300

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const path = require('path')
+const withSass = require('@zeit/next-sass');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+module.exports = withSass({cssModules: true
+})
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import nProgress from 'nprogress';
+import Script from 'next/script';
+import Router from 'next/router';
+import Link from 'next/link';
+import { Html } from 'next/document'; // eslint-disable-line
+
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Html lang='pt-br'>
+      <Head >
+        <title>Taylor App</title>
+        <meta name="description" content="Powered byJônatas Souza Soares" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="/styles/nprogress.css" />
+      </Head>
+
+      <Component {...pageProps} />
+
+      <Script src="/scripts/nprogress.js"></Script>
+    </Html>
+  )
 }
 
 export default MyApp
