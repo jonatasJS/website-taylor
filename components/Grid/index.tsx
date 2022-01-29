0887bfffb1b0a5558e84623b558a319fc77a0ee0
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2022-01-29 20:16:53 -0300

    instagram

--- components/Grid/index.tsx
+++ components/Grid/index.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.scss';
 function Grid() {
   return (
     <div className={styles.Grid}>
-      <Card href="https://www.instagram.com/taylorief/" title="Canal de Vendas" />
+      <Card href="https://www.instagram.com/taylorief/" title="Instagram" />
       <Card href="https://t.me/+M2TQPo5ZY4VjOGEx" title="Telegram" />
       <Card href="https://twitter.com/iefutebol1" title="Twitter" />
       <Card href="https://wa.me/message/W4NIZ7STXOAZI1" title="Whatsapp" />
